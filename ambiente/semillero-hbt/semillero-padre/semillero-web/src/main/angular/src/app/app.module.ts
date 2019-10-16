06250223e78312fb0d9f5c60c9db2202ae14c17e
dalvarez <diferalsi@gmail.com>
2019-10-15 20:54:56 -0500

    Se adiciona proyecto java-angular e instructivos

---
+++ ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule} from '@angular/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { APP_BASE_HREF } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpModule,
+    HttpClientModule
+  ],
+  providers: [
+  	{ provide: APP_BASE_HREF, useValue: '/SemilleroHBT' }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
